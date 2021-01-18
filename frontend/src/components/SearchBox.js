e7226e8eb729e1653dfe61fb552e9a64098ed322
Vladimir <ecofovka@gmail.com>
2021-01-18 19:54:01 +0100

    Add maim search auto complete functionality. Need styling & bag solving

--- frontend/src/components/SearchBox.js
+++ frontend/src/components/SearchBox.js
@@ -1,8 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Form, Button } from "react-bootstrap";
+import axios from "axios";
 
 const SearchBox = ({ history }) => {
   const [keyword, setKeyword] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  useEffect(() => {
+    const fetchSearchData = async () => {
+      try {
+        if (keyword.length > 0) {
+          const { data } = await axios.get(
+            `/api/products?keyword=${keyword}&pageNumber=1`
+          );
+          setSearchResults(data.products);
+        } else {
+          setSearchResults([]);
+        }
+      } catch (error) {
+        setSearchResults([]);
+      }
+    };
+    fetchSearchData();
+  }, [keyword]);
 
   const submitHandler = (e) => {
     e.preventDefault();
@@ -13,14 +33,30 @@ const SearchBox = ({ history }) => {
     }
   };
   return (
-    <Form onSubmit={submitHandler} inline>
+    <Form onSubmit={submitHandler} inline className="relative">
       <Form.Control
         type="text"
         name="q"
         onChange={(e) => setKeyword(e.target.value)}
         placeholder="Search Products..."
         className="mr-sm-2 ml-sm-5"
+        autoComplete="off"
       ></Form.Control>
+      {searchResults.length > 0 && (
+        <div className="autocomplete-box">
+          <ul>
+            {searchResults.map((suggestion) => (
+              <li
+                key={suggestion._id}
+                className="autocomplete-item"
+                onClick={() => history.push(`/product/${suggestion._id}`)}
+              >
+                {suggestion.name}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
       <Button type="submit" variant="outline-success" className="p-2">
         Search
       </Button>
