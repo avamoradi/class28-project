6476aeaf74a5e86e28e1f4201c6a9e830dea12ff
avamoradi <ava.moradi86@gmail.com>
2021-01-29 12:23:19 +0100

    add search event tracker to the search bar

--- frontend/src/components/SearchBox.js
+++ frontend/src/components/SearchBox.js
@@ -1,31 +1,35 @@
-import React, { useState } from "react";
-import { Form, Button } from "react-bootstrap";
+import React, { useState } from 'react'
+import { Form, Button } from 'react-bootstrap'
+import { useGAEventTracker } from '../analytic'
 
 const SearchBox = ({ history }) => {
-  const [keyword, setKeyword] = useState("");
+  const GAEventsTracker = useGAEventTracker('Search for item')
+
+  const [keyword, setKeyword] = useState('')
 
   const submitHandler = (e) => {
-    e.preventDefault();
+    GAEventsTracker('Clicked on search bar')
+    e.preventDefault()
     if (keyword.trim()) {
-      history.push(`/search/${keyword}`);
+      history.push(`/search/${keyword}`)
     } else {
-      history.push("/");
+      history.push('/')
     }
-  };
+  }
   return (
     <Form onSubmit={submitHandler} inline>
       <Form.Control
-        type="text"
-        name="q"
+        type='text'
+        name='q'
         onChange={(e) => setKeyword(e.target.value)}
-        placeholder="Search Products..."
-        className="mr-sm-2 ml-sm-5"
+        placeholder='Search Products...'
+        className='mr-sm-2 ml-sm-5'
       ></Form.Control>
-      <Button type="submit" variant="outline-success" className="p-2">
+      <Button type='submit' variant='outline-success' className='p-2'>
         Search
       </Button>
     </Form>
-  );
-};
+  )
+}
 
-export default SearchBox;
+export default SearchBox
