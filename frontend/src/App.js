11ec6fbdda203d3be8c98190d7d16d352f9dc166
Vladimir <ecofovka@gmail.com>
2021-02-03 13:41:40 +0100

    Solved conflicts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Subscribe from "./components/Subscribe";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
