30d43fbfa41be391fd0739efedab436d1d9d5c31
Omer Degirmenci <rumidegirmenci0@gmail.com>
2021-02-04 15:38:35 +0100

    add style

--- frontend/src/components/DropdownMenu.js
+++ frontend/src/components/DropdownMenu.js
@@ -5,45 +5,54 @@ import { DropdownSubmenu, NavDropdownMenu } from "react-bootstrap-submenu";
 
 const DropdownMenu = () => {
   return (
-    <Navbar>
-      <Container
-        sm={2}
-        bg={6}
-        lg={12}
-        className='sec-nav-container d-flex justify-content-center'
-      >
-        <LinkContainer to='/paintings'>
-          <Button className='sec-nav-btn' variant='secondary' size='md'>
-            <i className='fa fa-paint-brush' aria-hidden='true'></i>
-            Paintings
-          </Button>
-        </LinkContainer>
+    <>
+      <Navbar expand='lg' collapseOnSelect>
+        <Container className='sec-nav-container'>
+          <Navbar.Toggle
+            fluid
+            as={Button}
+            aria-controls='basic-navbar-nav'
+            className='navbar-toggle'
+          >
+            <i className='fas fa-chevron-down'></i>
+          </Navbar.Toggle>
+          <Navbar.Collapse
+            id='basic-navbar-nav'
+            className='justify-content-center'
+          >
+            <LinkContainer to='/paintings'>
+              <Button className='sec-nav-btn' variant='secondary' size='md'>
+                Paintings
+              </Button>
+            </LinkContainer>
 
-        <LinkContainer to='/photography'>
-          <Button className='sec-nav-btn' variant='secondary' size='md'>
-            <i className='fas fa-camera'></i>Photography
-          </Button>
-        </LinkContainer>
+            <LinkContainer to='/photography'>
+              <Button className='sec-nav-btn' variant='secondary' size='md'>
+                Photography
+              </Button>
+            </LinkContainer>
 
-        <LinkContainer to='/drawings'>
-          <Button className='sec-nav-btn' variant='secondary' size='md'>
-            <i className='fas fa-pencil-ruler'></i>Drawings
-          </Button>
-        </LinkContainer>
+            <LinkContainer to='/drawings'>
+              <Button className='sec-nav-btn' variant='secondary' size='md'>
+                Drawings
+              </Button>
+            </LinkContainer>
 
-        <LinkContainer to='/sculpture'>
-          <Button className='sec-nav-btn' variant='secondary' size='md'>
-            <i className='fas fa-portrait'></i>Sculpture
-          </Button>
-        </LinkContainer>
+            <LinkContainer to='/sculpture'>
+              <Button className='sec-nav-btn' variant='secondary' size='md'>
+                Sculpture
+              </Button>
+            </LinkContainer>
 
-        <LinkContainer to='/sell'>
-          <Button className='sec-nav-btn' variant='success' size='md'>
-            <i className='fas fa-money-check-alt'></i>Sell Art
-          </Button>
-        </LinkContainer>
-      </Container>
-    </Navbar>
+            <LinkContainer to='/sell'>
+              <Button className='sec-nav-btn' variant='success' size='md'>
+                Sell Art
+              </Button>
+            </LinkContainer>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
   );
 };
 
